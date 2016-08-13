154b56ace8614a939d485dfe28900bb1cf00ffb5
Ygor da Silva Vieira <ygor.vieira@medbarra.com.br>
2016-08-13 10:00:37 -0300

    Melhoria no envio e limpeza do formulário

--- HelpDesk/public/js/controllers/form-controller.js
+++ HelpDesk/public/js/controllers/form-controller.js
@@ -1,25 +1,26 @@
-var helpdesk = angular.module('helpdesk', ['ngMessages']).
-controller('formController', function($scope, $http) {
+var helpdesk = angular.module('helpdesk')
+.controller('formController', function($scope, $http) {
+
+$scope.formulario = {};
 
     $scope.submitForm = function() {
         if ($scope.formulario.$valid) {
             console.log($scope.cadastro);
             console.log('Formulario OK');
+            $scope.cadastro = {};
+            $scope.formulario.$setPristine();
         } else {
             console.log('erro!');
         }
     };
-    $scope.refresh = function() {
-        $http.get('/').success(function(response) {
-            console.log("Efetuada limpeza de campos");
-            $scope.cadastro = response;
-            $scope.submitForm = "";
-        });
-    };
-
-
-
-
+//    $scope.refresh = function() {
+//        $http.get('/').success(function(response) {
+//            console.log("Efetuada limpeza de campos");
+//            $scope.cadastro = response;
+//            $scope.submitForm = "";
+//            $scope.formulario.$setPristine();
+//        });
+//    };
 });
 
 
