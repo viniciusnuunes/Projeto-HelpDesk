f445dd63e833f9aa33a95973621446f027bec556
Ygor da Silva Vieira <ygor.vieira@medbarra.com.br>
2016-08-23 15:57:07 -0300

    correções no código do controller e na interação com o HTML

--- HelpDesk/public/js/controllers/form-controller.js
+++ HelpDesk/public/js/controllers/form-controller.js
@@ -1,4 +1,4 @@
-(function ($scope) {
+(function () {
 'use strict';
 
 angular
@@ -17,7 +17,6 @@ vm.limpaCampos = limpaCampos;
 
 function limpaCampos() {
   this.cadastro = {};
-  $scope.formulario.$setPristine();
   console.log("Limpando campos do formulário!");
 }
 
@@ -33,30 +32,3 @@ function submitForm() {
 }
 }
 })();
-
-// Versão anterior do código. Para utilizá-la, remover
-// o "as vm" da declaração do controller e o "vm."
-// antes de todas as chamadas de função na index.html
-
-/*angular.module('helpdesk')
-.controller('formController', function($scope, $http) {
-
-$scope.formulario = {};
-
-$scope.limpaCampos = function() {
-    $scope.cadastro = {};
-    $scope.formulario.$setPristine(); //Seta o formulário para o estado sem interação do usuário
-    console.log("Efetuada limpeza de campos");
-};
-
-    $scope.submitForm = function() {
-        if ($scope.formulario.$valid) {
-            console.log($scope.cadastro);
-            console.log('Formulario OK');
-            $scope.limpaCampos();
-        } else {
-            console.log('erro!');
-        }
-    };
-});
-*/
