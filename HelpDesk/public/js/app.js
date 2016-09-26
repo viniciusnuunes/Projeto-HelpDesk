cd0ef777492dd204eb649693a14e4f3f5dc1a59e
vinicius nunes <viniiciuspereiira@hotmail.com>
2016-09-26 20:31:22 -0300

    Refatoração da Index, rotas funcionando para cadastro e consulta (SPA)

--- HelpDesk/public/js/app.js
+++ HelpDesk/public/js/app.js
@@ -1,4 +1,18 @@
 // Criação do módulo principal do Angular.
 // Todas as injeções de dependências DEVEM ser feitas
 // neste arquivo!
-angular.module( 'helpdesk', ['ngMessages', 'ngRoute', 'ngResource'] );
+angular.module( 'helpdesk', ['ngMessages', 'ngRoute', 'ngResource'] )
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/cadastro',{
+        templateUrl:"views/cadastro/cadastro.html",
+        controller:"cadastroController",
+        controllerAs:"cadastroController"
+      })
+      .when('/consulta',{
+        templateUrl:"views/consulta/consulta.html",
+        controller:"cadastroController",
+        controllerAs:"cadastroController"
+      });
+    $routeProvider.otherwise({redirectTo: "/cadastro"});
+  });
