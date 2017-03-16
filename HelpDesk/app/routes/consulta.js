b2f99d251a60914da8d1fa7c114f0b3f7b6c1f92
Pedro Henrique Teixeira Braga <pedro.braga@medbarra.com.br>
2017-03-16 15:26:00 -0300

    Consertando a rota de consulta

--- HelpDesk/app/routes/consulta.js
+++ HelpDesk/app/routes/consulta.js
@@ -3,7 +3,7 @@ module.exports = function (app){
   var api = app.api.consulta;
 
   app.route ('v1/consultas')
-    .get(api.buscaPorId)
+//    .get(api.buscaPorId)
     //.delete(api.removePorId) a Implementar
     //.put(api.atualiza); a Implementar
 }
