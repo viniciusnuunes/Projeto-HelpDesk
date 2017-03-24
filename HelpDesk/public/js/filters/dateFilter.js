3db4a89114bf8c86b3c4974a77fbc8ea779f0f69
adrianorod <adriano.rodrigues@medgrupo.com.br>
2017-03-24 12:51:43 -0300

    Filtro de DATAS por período (início/fim) implementado CONSULTA.HTML

---
+++ HelpDesk/public/js/filters/dateFilter.js
@@ -0,0 +1,29 @@
+angular.module('helpdesk').filter("dateFilter", function($filter) {
+	return function( items, fromDate, toDate ) {
+		var filtered = [];
+		//here you will have your desired input
+		console.log(fromDate, toDate);
+
+		var from_date = "";
+		var to_date = "";
+
+		if(isNaN(fromDate) || fromDate===undefined || fromDate===null) {
+			from_date = 0;
+		} else {
+			from_date = Date.parse(fromDate);
+		}
+		if(isNaN(toDate) || toDate===undefined || toDate===null) {
+			to_date = 999999999999999999999;
+		} else {
+			to_date = Date.parse(toDate);
+		}
+
+		angular.forEach(items, function(item) {
+			var dataAtual = Date.parse(item.data);
+			if(dataAtual > from_date && dataAtual < to_date) {
+				filtered.push(item);
+			}
+		});
+		return filtered;
+	};
+});
