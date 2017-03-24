3640960c41ac86c20c44c9a2ec51df506f0fb892
Ygor da Silva Vieira <ygor.vieira@medbarra.com.br>
2017-03-24 17:19:32 -0300

    Padronização de código

--- HelpDesk/public/js/app.js
+++ HelpDesk/public/js/app.js
@@ -1,6 +1,3 @@
-// Criação do módulo principal do Angular.
-// Todas as injeções de dependências DEVEM ser feitas
-// neste arquivo!
 angular.module( 'helpdesk', ['ngMessages', 'ngRoute', 'ngResource', 'menuSuperiorDirective', 'angularUtils.directives.dirPagination'] )
   .config(function($routeProvider, $locationProvider, $httpProvider) {
 
--- HelpDesk/public/js/services/token-interceptor.js
+++ HelpDesk/public/js/services/token-interceptor.js
@@ -15,7 +15,7 @@
         var interceptor = {};
 
         interceptor.response = function(response) {
-            var token = response.headers('x-access-token'); //No lado do servidor usa-se [] em headers. No lado Angular, usa-se ()
+            var token = response.headers('x-access-token');
             if (token) {
                 console.log('Armazenando token recebido no navegador');
                 $window.sessionStorage.token = token;
@@ -35,7 +35,6 @@
 
         interceptor.responseError = function(rejection) {
             if (rejection != null && rejection.status == 401) {
-                //apagar o token invalido e redirecionar para a parcial de login
                 delete $window.sessionStorage.token;
                 $location.path('/login');
             }
